fdaf2d750e3db110ca0b04bc5dbf007087278f24
Landen Doty <landen.doty@outlook.com>
2024-01-01 16:05:53 -0600

    test and metrics w dhat

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate loader;
 
-use loader::test_load_binary;
+use loader::{load_binary};
 
 fn main() {
-    test_load_binary("/home/landen/pba-rs/loader/src/test/plain");
+    let bin = load_binary("/home/landen/pba-rs/loader/src/test/plain");
 }
