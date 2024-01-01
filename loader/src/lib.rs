cafc22137fe8e19361bf32f2c9c54131443a379b
Landen Doty <landen.doty@outlook.com>
2024-01-01 17:07:54 -0600

    adding bit width field

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -13,6 +13,7 @@ fn unpack_object<'a>(bin:&'a mut binary::Binary, obj: &File) {
     bin.arch_str = String::from(arch.to_str());
     bin.arch = arch;
     bin.entry = obj.entry();
+    bin.bits = if obj.is_64() { 64 } else { 32 }; 
 }
 
 // exposed fn to load a binary file and return a ptr to representative Binary struct on the heap
